799a6ef367cf6d3e482d5aeb9a56c6ef9414108a
gkmike <gkmike68@hotmail.com>
2023-04-19 18:41:30 +0800

    code refactored

--- shell_executor.py
+++ shell_executor.py
@@ -9,28 +9,29 @@ import concurrent.futures
 from datetime import datetime
 
 class Agent:
-    def __init__(self, ws, jobs, **kwargs):
-        self.boss = Boss(ws)
-        if "rerun_status" in kwargs:
-            self.boss.set_rerun_status(kwargs["rerun_status"])
+    def __init__(self, ws, jobs):
+        self.boss = Boss()
         self.ws = ws
         self.jobs = jobs
-        self.load_jobs(jobs.keys())
-    def load_jobs(self, selected_job_names):
+        self.load_jobs_by_names(jobs.keys())
+    def load_jobs_by_names(self, selected_job_names):
+        self.boss.reset()
         for job_name, job_data in self.jobs.items():
             if job_name not in selected_job_names:
                 continue
             w = Worker(job_name, job_data, self.ws)
             self.boss.hire_worker(w)
     def run(self, max_concurrent):
-        ws = self.boss.ws
+        ws = self.ws
         if not os.path.exists(ws):
             os.makedirs(ws)
         with open(f"{ws}/se_jobs.yaml", "w") as fp:
             yaml.dump(self.jobs, fp, sort_keys=False, default_flow_style=False)
-        self.boss.run_project(max_concurrent)
+        return self.boss.run(max_concurrent)
     def get_result_table(self):
         return self.boss.get_result_table()
+    def get_worker_report(self, job_name):
+        return self.boss.get_worker_report(job_name)
     def dump_csv(self, output_file):
         import pandas as pd
         df = pd.DataFrame(self.get_result_table())
@@ -43,13 +44,14 @@ class GUI:
     def __init__(self, agent):
         import pandas as pd
         import gradio as gr
-        df_ori = pd.DataFrame(agent.get_result_table())
 
-        def reload_jobs():
+        def get_jobs_df(text_filter=""):
             df = pd.DataFrame(agent.get_result_table())
             df["status"] = df["status"].apply(status_color)
             df["job_start_time"] = df["job_start_time"].apply(pre)
             show_col = ["job_name", "status", "job_start_time", "job_duration"] + [col for col in df.columns if 'env/' in col]
+            if text_filter.strip() != "":
+                df = df.query(text_filter)
             return df[show_col]
         def pre(val):
             return f"<pre>{val}</pre>"
@@ -62,38 +64,35 @@ class GUI:
             color = color_map.get(val, "gray")
             return f"<span style='color:{color}'>{val}</span>"
         def gui_run(df, max_workers):
-            agent.load_jobs(list(df["job_name"]))
-            agent.run(max_workers)
-            return "Done"
-        def get_df(text_filter):
-            df = df_ori.query(text_filter)
-            print(text_filter, df)
-            return df
+            agent.load_jobs_by_names(list(df["job_name"]))
+            return agent.run(max_workers)
         with gr.Blocks() as demo:
-            btn2 = gr.Button("Refresh Status Table")
-            df_filter = gr.Textbox(label="df filter", info="input filter for the following table for check and run")
-            gdf = gr.DataFrame(reload_jobs(), interactive=False, wrap=True)
-            gdf.datatype = "markdown"
+            gr_refresh_btn = gr.Button("Refresh Status Table")
+            gr_filter_textbox = gr.Textbox(label="df filter", info="input filter for the following table for check and run")
+            gr_df = gr.DataFrame(interactive=False, wrap=True)
+            gr_df.datatype = "markdown"
             with gr.Box():
                 gr.Markdown("Job Details")
                 with gr.Row():
-                    detail = gr.Code(language="yaml")
-                    console_log = gr.Code(language="shell")
+                    gr_detail_code = gr.Code(language="yaml")
+                    gr_console_code = gr.Code(language="shell")
             with gr.Box():
                 gr.Markdown("Run Controller")
                 with gr.Row():
-                    sld = gr.Slider(0, 1000, value=1, step=1, label="Max Workers")
-                    btn = gr.Button("Start")
-                    lb = gr.Label("Ready To Start")
-            df_filter.submit(get_df, inputs=[df_filter], outputs=[gdf])
-            btn.click(gui_run, inputs=[gdf, sld], outputs=[lb]).then(reload_jobs, outputs=[gdf])
-            btn2.click(reload_jobs, outputs=[gdf])
-            def gdf_select(evt: gr.SelectData):
+                    gr_nworks_slider = gr.Slider(0, 1000, value=1, step=1, label="Max Workers")
+                    gr_start_btn = gr.Button("Start")
+                    gr_status_label = gr.Label("Ready To Start")
+            gr_filter_textbox.submit(get_jobs_df, inputs=[gr_filter_textbox], outputs=[gr_df])
+            gr_start_btn.click(gui_run, inputs=[gr_df, gr_nworks_slider], outputs=[gr_status_label]).then(
+                    get_jobs_df, outputs=[gr_df], inputs=[gr_filter_textbox]
+            )
+            gr_refresh_btn.click(get_jobs_df, outputs=[gr_df], inputs=[gr_filter_textbox])
+            def gr_df_select(evt: gr.SelectData):
                 row = evt.index[0]
                 col = evt.index[1]
                 val = evt.value
                 if col == 0:
-                    report = agent.boss.workers[val].job_report()
+                    report = agent.get_worker_report(val)
                     yaml_out = yaml.dump(report, sort_keys=False, default_flow_style=False)
                     console_log_file = report["console_log"]
                     job_log = ""
@@ -102,19 +101,17 @@ class GUI:
                             job_log = fp.read()
                     return yaml_out, job_log
                 return "", ""
-            gdf.select(gdf_select, outputs=[detail, console_log])
-            demo.load(reload_jobs, outputs=[gdf])
+            gr_df.select(gr_df_select, outputs=[gr_detail_code, gr_console_code])
+            demo.load(get_jobs_df, outputs=[gr_df], inputs=[gr_filter_textbox])
         demo.launch(inbrowser=True)
 
 class Boss:
-    def __init__(self, ws):
+    def __init__(self):
         self.workers = {}
         self.todo_workers = []
-        self.ws = ws
-        self.rerun_status = ["ERROR"]
     def reset(self):
         self.workers = {}
-        self.todo_workers = {}
+        self.todo_workers = []
     def hire_worker(self, worker):
         self.workers[worker.job_name] = worker
         self.todo_workers.append(worker)
@@ -123,8 +120,11 @@ class Boss:
         for w in self.workers.values():
             result.append(w.job_table())
         return result
-    def run_project(self, max_concurrent):
+    def get_worker_report(self, job_name):
+        return self.workers[job_name].job_table()
+    def run(self, max_concurrent):
         while len(self.todo_workers) > 0:
+            complete_jobs = 0
             with concurrent.futures.ThreadPoolExecutor(max_workers=max_concurrent) as executor:
                 futures = []
                 for w in self.todo_workers[:]:
@@ -132,14 +132,17 @@ class Boss:
                         if self.workers[w.dep].status != "DONE":
                             continue
                     self.todo_workers.remove(w)
-                    if w.status not in self.rerun_status + [""]:
+                    complete_jobs += 1
+                    if w.status == "DONE":
                         continue
                     w.setup_cwd()
                     future = executor.submit(w.act)
                     futures.append(future)
                 [future.result() for future in futures]
-    def set_rerun_status(self, status_list):
-        self.rerun_status = status_list
+            if complete_jobs == 0:
+                print("Can't complete any job due to dependency error")
+                return "Dependency Errors"
+        return "Done"
 
 class Worker:
     def __init__(self, job_name, job_data, ws):
